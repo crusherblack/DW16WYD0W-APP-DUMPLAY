d34dfc510530026ad72bdbc3870f1272cf3898af
crusherblack <fadhildarma13@gmail.com>
2020-06-23 16:08:35 +0700

    fix hero padding

--- client/src/components/HeroImage/HeroImage.js
+++ client/src/components/HeroImage/HeroImage.js
@@ -1,29 +1,29 @@
-import React from "react";
-import "./HeroImage.css";
-import img from "../../img/videothumbnail/music.png";
+import React from 'react';
+import './HeroImage.css';
+import img from '../../img/videothumbnail/music.png';
 
 const HeroImage = () => {
-  return (
-    <div>
-      <div
-        className="hero"
-        style={{
-          height: "70vh",
-          background: `url('${img}'), #1c1c1c`,
-        }}
-      ></div>
-      <div className="hero-content">
-        <div className="hero-text">
-          <h1>Connect on DumbSound</h1>
-          <p>
-            Discovery, Stream, and share a constantly expanding mix of music{" "}
-            <br />
-            from emerging and major artists around the world
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div
+				className="hero"
+				style={{
+					height: '70vh',
+					background: `url('${img}'), #1c1c1c`
+				}}
+			/>
+			<div className="hero-content">
+				<div className="hero-text">
+					<h1>Connect on DumbSound</h1>
+					<p>
+						Discovery, Stream, and share a constantly expanding mix of music{' '}
+						<br />
+						from emerging and major artists around the world
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HeroImage;
