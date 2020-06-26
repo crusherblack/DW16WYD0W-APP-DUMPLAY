516f3d5d137b7a60eb5aa431a1a90c9229301fc1
fadhil darma putera zm <fadhildarma13@gmail.com>
2020-06-26 10:42:00 +0700

    add notif

--- client/src/pages/Crud/AddArtist.js
+++ client/src/pages/Crud/AddArtist.js
@@ -1,128 +1,132 @@
-import React, { useState } from 'react';
-import '../css/Movie.css';
+import React, { useState } from "react";
+import "../css/Movie.css";
 
-import { add_Artist } from '../../redux/actions/music';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import { add_Artist } from "../../redux/actions/music";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 
-const AddArtist = ({ add_Artist }) => {
-	let history = useHistory();
+const AddArtist = ({ add_Artist, music: { alert } }) => {
+  let history = useHistory();
 
-	const [ formData, setFormData ] = useState({
-		name: '',
-		old: '',
-		type: '',
-		startCareer: ''
-	});
+  const [formData, setFormData] = useState({
+    name: "",
+    old: "",
+    type: "",
+    startCareer: "",
+  });
 
-	const onChange = (event) => {
-		const updateForm = { ...formData };
-		updateForm[event.target.name] =
-			event.target.type === 'file' ? event.target.files[0] : event.target.value;
-		setFormData(updateForm);
-	};
+  const onChange = (event) => {
+    const updateForm = { ...formData };
+    updateForm[event.target.name] =
+      event.target.type === "file" ? event.target.files[0] : event.target.value;
+    setFormData(updateForm);
+  };
 
-	const { name, old, startCareer } = formData;
+  const { name, old, startCareer } = formData;
 
-	const cleanForm = () => {
-		setFormData({
-			name: '',
-			old: '',
-			type: '',
-			startCareer: ''
-		});
-	};
+  const cleanForm = () => {
+    setFormData({
+      name: "",
+      old: "",
+      type: "",
+      startCareer: "",
+    });
+  };
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		add_Artist(formData, cleanForm);
-	};
+  const onSubmit = (e) => {
+    e.preventDefault();
+    add_Artist(formData, cleanForm);
+  };
 
-	return (
-		<div className="container">
-			<form onSubmit={(e) => onSubmit(e)}>
-				<div className="list-movie">
-					<h2>Add Artist</h2>
-					<br />
-					<div className="form-group">
-						<input
-							type="text"
-							className="custom-input"
-							placeholder="Artist Name"
-							onChange={(e) => onChange(e)}
-							value={name}
-							name="name"
-							required
-						/>
-					</div>
-					<div>
-						<input
-							type="number"
-							className="custom-input"
-							placeholder="Old"
-							onChange={(e) => onChange(e)}
-							value={old}
-							name="old"
-							required
-						/>
-					</div>
-					<div className="form-group">
-						<select
-							name="type"
-							className="custom-select"
-							onChange={(e) => {
-								onChange(e);
-							}}
-							required
-						>
-							<option value="">Type</option>
-							<option value="Solo">Solo</option>
-							<option value="Group">Group</option>
-						</select>
-					</div>
-					<div className="form-group">
-						<input
-							type="number"
-							className="custom-input"
-							placeholder="Start A Career"
-							onChange={(e) => onChange(e)}
-							value={startCareer}
-							name="startCareer"
-							required
-						/>
-					</div>
+  return (
+    <div className="container">
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="list-movie">
+          <div style={{ textAlign: "center" }}>
+            {alert && <h4 style={{ color: "#EE4622" }}>{alert}</h4>}
+          </div>
 
-					<div
-						className="form-group"
-						style={{
-							textAlign: 'center'
-						}}
-					>
-						<button
-							type="submit"
-							className="btn-merah"
-							style={{
-								marginTop: '1em',
-								maxWidth: '25rem',
-								position: 'relative'
-							}}
-						>
-							Add Artist
-						</button>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+          <h2>Add Artist</h2>
+          <br />
+          <div className="form-group">
+            <input
+              type="text"
+              className="custom-input"
+              placeholder="Artist Name"
+              onChange={(e) => onChange(e)}
+              value={name}
+              name="name"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              className="custom-input"
+              placeholder="Old"
+              onChange={(e) => onChange(e)}
+              value={old}
+              name="old"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <select
+              name="type"
+              className="custom-select"
+              onChange={(e) => {
+                onChange(e);
+              }}
+              required
+            >
+              <option value="">Type</option>
+              <option value="Solo">Solo</option>
+              <option value="Group">Group</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              type="number"
+              className="custom-input"
+              placeholder="Start A Career"
+              onChange={(e) => onChange(e)}
+              value={startCareer}
+              name="startCareer"
+              required
+            />
+          </div>
+
+          <div
+            className="form-group"
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <button
+              type="submit"
+              className="btn-merah"
+              style={{
+                marginTop: "1em",
+                maxWidth: "25rem",
+                position: "relative",
+              }}
+            >
+              Add Artist
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 AddArtist.propTypes = {
-	add_Artist: PropTypes.func.isRequired
+  add_Artist: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	film: state.film
+  music: state.music,
 });
 
 export default connect(mapStateToProps, { add_Artist })(AddArtist);
--- client/src/redux/reducers/music.js
+++ client/src/redux/reducers/music.js
@@ -1,58 +1,61 @@
 import {
-	GET_MUSIC_ALL,
-	GET_MUSIC_FAIL,
-	ADD_ARTIST_SUCCESS,
-	ADD_ARTIST_FAIL,
-	ADD_MUSIC_SUCCESS,
-	ADD_MUSIC_FAIL,
-	GET_ARTIS_ALL,
-	GET_ARTIS_FAIL
-} from '../actions/types';
+  GET_MUSIC_ALL,
+  GET_MUSIC_FAIL,
+  ADD_ARTIST_SUCCESS,
+  ADD_ARTIST_FAIL,
+  ADD_MUSIC_SUCCESS,
+  ADD_MUSIC_FAIL,
+  GET_ARTIS_ALL,
+  GET_ARTIS_FAIL,
+} from "../actions/types";
 
 const initialState = {
-	loading: true,
-	musicAll: [],
-	artisAll: [],
-	error: ''
+  loading: true,
+  musicAll: [],
+  artisAll: [],
+  error: "",
+  alert: "",
 };
 
-export default function(state = initialState, actions) {
-	const { type, payload } = actions;
+export default function (state = initialState, actions) {
+  const { type, payload } = actions;
 
-	switch (type) {
-		case GET_MUSIC_ALL:
-			return {
-				...state,
-				musicAll: payload,
-				loading: false
-			};
-		case GET_ARTIS_ALL:
-			return {
-				...state,
-				artisAll: payload,
-				loading: false
-			};
-		case ADD_ARTIST_SUCCESS:
-			return {
-				...state,
-				loading: false
-			};
-		case ADD_MUSIC_SUCCESS:
-			return {
-				...state,
-				loading: false
-			};
-		case GET_MUSIC_FAIL:
-		case ADD_ARTIST_FAIL:
-		case ADD_MUSIC_FAIL:
-		case GET_ARTIS_FAIL:
-			return {
-				...state,
-				error: payload,
-				loading: false
-			};
+  switch (type) {
+    case GET_MUSIC_ALL:
+      return {
+        ...state,
+        musicAll: payload,
+        loading: false,
+      };
+    case GET_ARTIS_ALL:
+      return {
+        ...state,
+        artisAll: payload,
+        loading: false,
+      };
+    case ADD_ARTIST_SUCCESS:
+      return {
+        ...state,
+        alert: "Artis has been successfully added",
+        loading: false,
+      };
+    case ADD_MUSIC_SUCCESS:
+      return {
+        ...state,
+        alert: "Music has been successfully added",
+        loading: false,
+      };
+    case GET_MUSIC_FAIL:
+    case ADD_ARTIST_FAIL:
+    case ADD_MUSIC_FAIL:
+    case GET_ARTIS_FAIL:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
