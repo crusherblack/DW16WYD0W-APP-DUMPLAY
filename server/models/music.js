9b4900ada1bc6d25ac871fe4419338b5d7b562c7
crusherblack <fadhildarma13@gmail.com>
2020-07-02 14:27:27 +0700

    fix css

--- server/models/music.js
+++ server/models/music.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Music = sequelize.define("Music", {
-    title: DataTypes.STRING,
-    thumbnail: DataTypes.STRING,
-    year: DataTypes.STRING,
-    singerId: DataTypes.INTEGER,
-    attache: DataTypes.STRING,
-  });
-  Music.associate = function (models) {
-    Music.belongsTo(models.Artis, {
-      as: "artis",
-      foreignKey: {
-        name: "singerId",
-      },
-    });
-  };
-  return Music;
+	const Music = sequelize.define('Music', {
+		title: DataTypes.STRING,
+		thumbnail: DataTypes.STRING,
+		year: DataTypes.STRING,
+		singerId: DataTypes.INTEGER,
+		attache: DataTypes.STRING
+	});
+	Music.associate = function(models) {
+		Music.belongsTo(models.Artis, {
+			as: 'artis',
+			foreignKey: {
+				name: 'singerId'
+			}
+		});
+	};
+	return Music;
 };
