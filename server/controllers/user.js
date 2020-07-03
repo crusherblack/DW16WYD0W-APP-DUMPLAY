7ddc9d4334b8284e3555eadb15e46004132d0a4e
fadhil darma putera zm <fadhildarma13@gmail.com>
2020-07-03 09:15:25 +0700

    oke

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,69 +1,68 @@
-const {
-	User
-} = require('../models');
-const Joi = require('@hapi/joi');
+const { User } = require("../models");
+const Joi = require("@hapi/joi");
 
 exports.getUser = async (req, res) => {
-	try {
-		const user = await User.findAll({
-			attributes: {
-				exclude: ['password']
-			}
-		});
+  try {
+    const user = await User.findAll({
+      attributes: {
+        exclude: ["password"],
+      },
+    });
 
-		if (user) {
-			return res.send({
-				data: user
-			});
-		} else {
-			return res.status(500).send({
-				message: 'Server Error'
-			});
-		}
-	} catch (error) {
-		console.log(error);
-		return res.status(500).send({
-			error: {
-				message: 'Server Error'
-			}
-		});
-	}
+    if (user) {
+      return res.status(200).send({
+        status: "success",
+        data: user,
+      });
+    } else {
+      return res.status(500).send({
+        message: "Server Error",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      error: {
+        message: "Server Error",
+      },
+    });
+  }
 };
 
 exports.deleteUser = async (req, res) => {
-	try {
-		const user = await User.findOne({
-			where: {
-				id
-			}
-		});
+  try {
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
 
-		if (user) {
-			const deleteUser = await User.destroy({
-				where: {
-					id
-				}
-			});
+    if (user) {
+      const deleteUser = await User.destroy({
+        where: {
+          id,
+        },
+      });
 
-			return res.send({
-				data: {
-					id
-				}
-			});
-		} else {
-			return res.status(400).send({
-				error: {
-					message: 'User Not Found'
-				}
-			});
-		}
-	} catch (error) {
-		console.log(error);
-		return res.status(500).send({
-			error: {
-				message: 'Server Error'
-			}
-		});
-	}
-
+      return res.send({
+        status: "success",
+        data: {
+          id,
+        },
+      });
+    } else {
+      return res.status(400).send({
+        error: {
+          message: "User Not Found",
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      error: {
+        message: "Server Error",
+      },
+    });
+  }
 };
